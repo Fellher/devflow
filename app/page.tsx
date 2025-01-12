33bdef0fe762ae9bf15ffd8fe8012cb42e17d570
Felix Mauncho <39988041+Fellher@users.noreply.github.com>
2025-01-12 17:01:37 +0300

    set up and configured eslint

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 const  Home = ()=> {
   return (
     <>
-      <h1 className="text-3xl font-black text-white ">My Homepage</h1>
+      <h1 className="text-3xl text-white font-black ">My Homepage</h1>
       <p>Welcome to my homepage!</p>
     </>
   );
