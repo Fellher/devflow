ce1455c7829dbe61826bfa4e3d0daef228fa1757
Felix Mauncho <39988041+Fellher@users.noreply.github.com>
2025-01-12 18:05:44 +0300

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@
 const  Home = ()=> {
   return (
     <>
-      <h1 className="text-3xl text-white font-black ">My Homepage</h1>
+      <h1 className="text-3xl text-dark font-black ">My Homepage</h1>
+      <h1 className="text-3xl text-dark font-black font-space-grotesk ">My Homepage</h1>
       <p>Welcome to my homepage!</p>
     </>
   );
