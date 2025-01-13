91bc0f87220a04a77e51f0c40d1de59b2ab1a8f1
Felix Mauncho <39988041+Fellher@users.noreply.github.com>
2025-01-13 22:42:36 +0300

    set up light the top nav bar and dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-
-
-const  Home = ()=> {
+const Home = () => {
   return (
     <>
-      <h1 className="text-3xl text-dark font-black ">My Homepage</h1>
-      <h1 className="text-3xl text-dark font-black font-space-grotesk ">My Homepage</h1>
+      <h1 className="text-dark text-3xl font-black ">My Homepage</h1>
+      <h1 className="text-dark font-space-grotesk text-3xl font-black ">
+        My Homepage
+      </h1>
       <p>Welcome to my homepage!</p>
     </>
   );
-}
+};
 export default Home;
